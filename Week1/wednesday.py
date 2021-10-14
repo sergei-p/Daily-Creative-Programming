64edb6abf49b6316b3db1961101db83ac247bdcf
sergei-p <sergeidev2494@gmail.com>
2021-10-13 18:02:18 -0700

    Made entry for Wednesday

---
+++ Week1/wednesday.py
@@ -0,0 +1,16 @@
+theSet = set([2,4,5,1,10,7,20]) 
+print(theSet)
+
+newSet = set()
+newSet.add(2)
+newSet.add(10)
+newSet.add(4) 
+print(newSet)
+
+newList1 = [2,10,4,10,7,20,14,100]
+newList1.sort()
+print(newList1)
+newSet1 = set(newList1)
+
+print(newSet1)
+
